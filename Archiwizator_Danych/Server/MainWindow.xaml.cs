2e704dbc36725f2c61d0dd96d201b58117946d11
kpaluch26 <kamil.paluch98@gmail.com>
2021-03-16 23:31:34 +0100

    Stworzenie aplikacji serwera

---
+++ Archiwizator_Danych/Server/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes;
+using MaterialDesignColors;
+
+namespace Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            //this.Close(); //zamknięcie okna
+            Application.Current.Shutdown(); //zamknięcie aplikacji
+        }
+
+        private void btn_MinimizeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; //zminimalizowanie okna
+        }
+
+        private void btn_MenuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            btn_MenuOpen.Visibility = Visibility.Collapsed;
+            btn_MenuClose.Visibility = Visibility.Visible;
+        }
+
+        private void btn_MenuClose_Click(object sender, RoutedEventArgs e)
+        {
+            btn_MenuOpen.Visibility = Visibility.Visible;
+            btn_MenuClose.Visibility = Visibility.Collapsed;
+        }
+    }
+}
