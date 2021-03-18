b20728bccfef054ae7ea52e62c91d8db78e1abdc
kpaluch26 <kamil.paluch98@gmail.com>
2021-03-18 17:57:25 +0100

    Dodanie pozostalych kart dla konfiguracji serwera

--- Archiwizator_Danych/Server/MainWindow.xaml.cs
+++ Archiwizator_Danych/Server/MainWindow.xaml.cs
@@ -14,6 +14,10 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MaterialDesignThemes;
 using MaterialDesignColors;
+using System.IO;
+using System.Net;
+using Microsoft.Win32;
+using System.IO.Compression;
 
 namespace Server
 {
@@ -22,6 +26,8 @@ namespace Server
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ServerConfiguration config;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -74,6 +80,175 @@ namespace Server
         private void btn_ProfilPanel_Click(object sender, RoutedEventArgs e)
         {
             grd_Configuration.Visibility = Visibility.Hidden;
+        }
+
+        private void btn_ConfigurationLoad_Click(object sender, RoutedEventArgs e)
+        {
+            StreamReader file; //zmienna do odczytu pliku
+            string[] result = new string[2]; //tablica zmiennych do odczytu konfiguracji
+            string filePath, line, archive_address = ""; //zmienne pomocnicze do konfiguracji serwera
+            int port = 0, buffer_size = 0, counterp = 0, counterb = 0, countera = 0; //zmienne pomocnicze sprawdzające poprawność importowanych danych
+            string username = Environment.UserName; //odczyt nazwy konta użytkownika
+            string hostName = Dns.GetHostName(); //odczyt hostname
+            string ip_address = Dns.GetHostByName(hostName).AddressList[0].ToString(); // odczyt adresu IPv4
+
+            OpenFileDialog ofd = new OpenFileDialog(); //utworzenie okna do przeglądania plików konfiguracji
+            ofd.Filter = "txt files (*.txt)|*.txt|xml files (*.xml)|*.xml"; //ustawienie filtrów okna na pliki txt i xml
+            ofd.FilterIndex = 1; //ustawienie domyślnego filtru na plik txt
+            ofd.RestoreDirectory = true; //przywracanie wcześniej zamkniętego katalogu
+
+            if (ofd.ShowDialog() == true) //wyświetlenie okna ze sprawdzeniem, czy plik został wybrany
+            {
+                filePath = ofd.FileName; //przypisanie ścieżki wybranego pliku do zmiennej
+
+                if (ofd.FilterIndex == 1)//odczyt dla pliku txt
+                {
+                    try
+                    {
+                        file = new StreamReader(filePath); //utworzenie odczytu pliku
+                        while ((line = file.ReadLine()) != null) //dopóki są linie w pliku
+                        {
+                            line = String.Concat(line.Where(x => !Char.IsWhiteSpace(x))); //usunięcie wszelkich znaków białych z linii
+                            result = line.Split('='); //podzielenie odczytanej linii wykorzystując separator
+                            switch (result[0].ToLower()) //zmiana liter na małe w poleceniu
+                            {
+                                case "port_tcp": //polecenie
+                                    port = Convert.ToInt32(result[1].Substring(1, result[1].Length - 2)); //przypisanie numeru portu odczytanego z pliku txt
+                                    counterp = 1; //poprawny format
+                                    break;
+                                case "buffer_size": //polecenie
+                                    buffer_size = Convert.ToInt32(result[1].Substring(1, result[1].Length - 2)); //przypisanie rozmiaru buffera odczytanego z pliku txt
+                                    counterb = 1; //poprawny format
+                                    break;
+                                case "archive_address": //polecenie
+                                    archive_address = Convert.ToString(result[1].Substring(1, result[1].Length - 2)); //przypisanie ścieżki zapisu otrzymanych plików
+                                    countera = 1; //poprawny format
+                                    break;
+                            }
+                            if (counterp + counterb + countera == 3) //jeśli wczytano wszystkie niezbędne dane
+                            {
+                                break; //przerwij dalsze wczytywanie
+                            }
+                        }
+                        file.Close(); //zamknięcie pliku
+                        if (counterp + counterb + countera != 3) //jeśli nie wczytano wszystkich niezbędnych danych
+                        {
+                            file.Close(); //zamknięcie pliku
+                            throw new FileLoadException(); //wyrzucenie wyjątku
+                        }
+                        config = new ServerConfiguration(username, hostName, ip_address, archive_address, port, buffer_size);//utworzenie configa    
+                        pic_ConfigurationLoad.Kind = MaterialDesignThemes.Wpf.PackIconKind.Check; //zmiana ikony na powodzenie operacji
+                    }
+                    catch (FileLoadException)
+                    { 
+                        pic_ConfigurationLoad.Kind = MaterialDesignThemes.Wpf.PackIconKind.Close; //zmiana ikony na niepowodzenie operacji
+                    }
+                }
+                else if (ofd.FilterIndex == 2)//odczyt dla pliku xml
+                {
+                    try
+                    {
+                        bool serwer = false; //zmienna pomocnicza do odczytu konfiguracji dla serwera
+                        bool configure = false; //zmienna pomocnicza sprawdzająca poprawność importowanych danych
+                        file = new StreamReader(filePath); //utworzenie odczytu pliku
+                        while ((line = file.ReadLine()) != null) //dopóki są linie w pliku
+                        {
+                            line = String.Concat(line.Where(x => !Char.IsWhiteSpace(x))); //usunięcie wszelkich znaków białych z linii
+                            if (line.ToLower() == "<serwer>") //początek konfiguracji serwera
+                            {
+                                serwer = true; //ustawienie odczytu danych dla serwera
+                            }
+                            else if (line.ToLower() == "</serwer>") //koniec konfiguracji serwera
+                            {
+                                serwer = false; //przerwanie odczytu danych
+                                break;
+                            }
+                            else if (line.ToLower() == "<configure>" && serwer) //początek konfiguracji
+                            {
+                                configure = true; //ustawienie odczytu konfiguracji
+                            }
+                            else if (line.ToLower() == "</configure>" && serwer) //koniec konfiguracji
+                            {
+                                configure = false; //przerwanie konfiguracji
+                            }
+                            else if (serwer && configure) //jeśli konfiguracja obowiązuje dla serwera
+                            {
+                                result = line.Split('='); //podzielenie odczytanej linii wykorzystując separator
+                                switch (result[0].ToLower()) //ustawienie małych liter poleceń
+                                {
+                                    case "port_tcp": //polecenie
+                                        port = Convert.ToInt32(result[1].Substring(1, result[1].Length - 2)); //przypisanie numeru portu odczytanego z pliku xml
+                                        counterp = 1; //poprawny format
+                                        break;
+                                    case "buffer_size": //polecenie
+                                        buffer_size = Convert.ToInt32(result[1].Substring(1, result[1].Length - 2)); //przypisanie numeru portu odczytanego z pliku xml
+                                        counterb = 1; //poprawny format
+                                        break;
+                                    case "archive_address": //polecenie 
+                                        archive_address = Convert.ToString(result[1].Substring(1, result[1].Length - 2)); //przypisanie ścieżki zapisu otrzymanych plików
+                                        countera = 1; //poprawny format
+                                        break;
+                                }
+                            }
+                        }
+                        file.Close(); //zamknięcie pliku
+                        if (counterp + counterb + countera != 3) //jeśli niepoprawny format konfiguracji
+                        {
+                            file.Close(); //zamknięcie pliku
+                            throw new FileLoadException(); //wyrzuca wyjątek
+                        }
+                        config = new ServerConfiguration(username, hostName, ip_address, archive_address, port, buffer_size);//utworzenie configa 
+                        pic_ConfigurationLoad.Kind = MaterialDesignThemes.Wpf.PackIconKind.Check; //zmiana ikony na powodzenie operacji
+                    }
+                    catch (FileLoadException)
+                    {
+                        pic_ConfigurationLoad.Kind = MaterialDesignThemes.Wpf.PackIconKind.Close; //zmiana ikony na niepowodzenie operacji
+                    }
+                }
+            }
+            else
+            {
+                pic_ConfigurationLoad.Kind = MaterialDesignThemes.Wpf.PackIconKind.Close; //zmiana ikony na niepowodzenie operacji
+            }
+        }
+
+        private void btn_ConfigurationSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfg = new SaveFileDialog(); //utworzenie okna do przeglądania plików
+            sfg.Filter = "txt files (*.txt)|*.txt|xml files (*.xml)|*.xml"; //ustawienie filtrów okna na pliki txt i xml
+            sfg.FilterIndex = 1; //ustawienie domyślnego filtru na plik txt
+            sfg.RestoreDirectory = true; //przywracanie wcześniej zamkniętego katalogu
+
+            if (sfg.ShowDialog() == true)//wyświetlenie okna ze sprawdzeniem, czy plik został zapisany
+            {
+                if (sfg.FilterIndex == 1) //zapis dla pliku txt
+                {
+                    File.WriteAllText(sfg.FileName, "port_tcp=" + '"' + config.GetPort() + '"' +
+                    Environment.NewLine + "buffer_size=" + '"' + config.GetBufferSize() + '"' +
+                    Environment.NewLine + "archive_address=" + '"' + config.GetArchiveAddress() + '"'); //stworzenie lub nadpisanie pliku        
+                }
+                else if (sfg.FilterIndex == 2) //zapis dla pliku xml
+                {
+                    File.WriteAllText(sfg.FileName, "<serwer>" +
+                        Environment.NewLine + "    <configure>" +
+                        Environment.NewLine + "        port_tcp=" + '"' + config.GetPort() + '"' +
+                        Environment.NewLine + "        buffer_size=" + '"' + config.GetBufferSize() + '"' +
+                        Environment.NewLine + "        archive_address=" + '"' + config.GetArchiveAddress() + '"' +
+                        Environment.NewLine + "    </configure>" +
+                        Environment.NewLine + "</serwer>"); //stworzenie lub nadpisanie pliku 
+                }
+                pic_ConfigurationSave.Kind = MaterialDesignThemes.Wpf.PackIconKind.Check; //zmiana ikony na powodzenie operacji
+            }
+            else
+            {
+                Console.WriteLine("Nie zapisano pliku. Powrót do menu głównego."); //komunikat
+                pic_ConfigurationSave.Kind = MaterialDesignThemes.Wpf.PackIconKind.Close; //zmiana ikony na niepowodzenie operacji
+            }
+        }
+
+        private void btn_ConfigurationCreate_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Archiwizator_Danych/Server/ServerConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    class ServerConfiguration
+    {
+        private string username;
+        private string hostname;
+        private string ip_address;
+        private string archive_address;
+        private int port;
+        private int buffer_size;
+
+        public ServerConfiguration(string u, string h, string i, string a, int p, int b)
+        {
+            this.username = u;
+            this.ip_address = i;
+            this.port = p;
+            this.buffer_size = b;
+            this.hostname = h;
+            this.archive_address = a;
+        }
+
+        public int GetPort()
+        {
+            return this.port;
+        }
+
+        public int GetBufferSize()
+        {
+            return this.buffer_size;
+        }
+
+        public string GetArchiveAddress()
+        {
+            return this.archive_address;
+        }
+
+        public string SetArchiveAddress(string a)
+        {
+            this.archive_address = a;
+            return this.archive_address;
+        }
+
+        public override string ToString()
+        {
+            return $" Użytkownik: {username} || AdresIPv4: {ip_address} || Hostname: {hostname} ||  Port: {port} || Rozmiar bufera: {buffer_size} || Ścieżka: {archive_address} ";
+        }
+    }
+}
